5a9722f2872939e65e4f1dd4300225a5119ab362
ManhBanh <52659954+ManhBanh@users.noreply.github.com>
2019-07-26 21:36:40 +0700

    Trần Quang Mạnh

--- Login/app/src/main/java/vn/edu/devpro/login/MainActivity.java
+++ Login/app/src/main/java/vn/edu/devpro/login/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 import vn.edu.devpro.login.databinding.ActivityMainBinding;
 
@@ -18,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
     SQLHelper sqlHelper;
     ArrayList<Account> accountArrayList;
     Account account1, account2, account3;
+    final Pattern patternSo = Pattern.compile("^" + "(?=.*[0-9])" + ".{6,}" + "$");
+    final Pattern patternChuHoa = Pattern.compile("^" + "(?=.*[A-Z])" + ".{6,}" + "$");
+    final Pattern patternChuThuong = Pattern.compile("^" + "(?=.*[a-z])" + ".{6,}" + "$");
+    final Pattern patternKiTuDacBiet = Pattern.compile("^" + "(?=.*[@#$%!&*_+=])" + ".{6,}" + "$");
+    final Pattern patternKhoangTrang = Pattern.compile("^" + "(?=\\S+$)" + ".{6,}" + "$");
+//    final String abc = "((?=.*[a-z]))";
 
     ActivityMainBinding binding;
     @Override
@@ -28,14 +35,16 @@ public class MainActivity extends AppCompatActivity {
         
         sqlHelper = new SQLHelper(getBaseContext());
 
-//        sqlHelper.insertIntoTable("tranmanh", "Matkhau_01");
-
         accountArrayList = sqlHelper.getAllAccount();
+
+
+//        pattern = Pattern.compile("(?=.*[0-9])");
 
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             int dem = 0;
             @Override
             public void onClick(View view) {
+                String password = binding.ePassword.getText().toString();
                 if(binding.eUsername.getText().length() == 0 || binding.ePassword.getText().length() == 0){
                     Toast.makeText(getBaseContext(), "Vui lòng điền đầy đủ thông tin", Toast.LENGTH_SHORT).show();
                 }
@@ -44,26 +53,42 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(getBaseContext(), "Mật khẩu phải có ít nhất 6 kí tự.", Toast.LENGTH_SHORT).show();
                     }
                     else {
-                        if (accountArrayList != null) {
-                            for (int i = 0; i < accountArrayList.size(); i++) {
-                                if ((binding.eUsername.getText().toString().equals(accountArrayList.get(i).getUsername()) == true)
-                                        && (binding.ePassword.getText().toString().equals(accountArrayList.get(i).getPassword()) == true)) {
+                        if(!patternSo.matcher(password).matches()) {
+                            Toast.makeText(getBaseContext(), "Mật khẩu phải bao gồm ít nhất một số.", Toast.LENGTH_SHORT).show();
+                        }
+                        else if(!patternChuHoa.matcher(password).matches()){
+                            Toast.makeText(getBaseContext(), "Mật khẩu phải bao gồm ít nhất một chữ hoa.", Toast.LENGTH_SHORT).show();
+                        }
+                        else if(!patternChuThuong.matcher(password).matches()){
+                            Toast.makeText(getBaseContext(), "Mật khẩu phải bao gồm ít nhất một chữ thường.", Toast.LENGTH_SHORT).show();
+                        }
+                        else if(!patternKiTuDacBiet.matcher(password).matches()){
+                            Toast.makeText(getBaseContext(), "Mật khẩu phải bao gồm ít nhất một kí tự đặc biệt.", Toast.LENGTH_SHORT).show();
+                        }
+                        else if(!patternKhoangTrang.matcher(password).matches()){
+                            Toast.makeText(getBaseContext(), "Mật khẩu không được có khoảng trắng.", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            if (accountArrayList != null) {
+                                for (int i = 0; i < accountArrayList.size(); i++) {
+                                    if ((binding.eUsername.getText().toString().equals(accountArrayList.get(i).getUsername()) == true)
+                                            && (password.equals(accountArrayList.get(i).getPassword()) == true)) {
 
-                                    dem = 1;
-                                    break;
+                                        dem = 1;
+                                        break;
+                                    }
                                 }
+                                if (dem != 1) {
+                                    Toast.makeText(getBaseContext(), "Tài khoản hoặc mật khẩu không chính xác!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getBaseContext(), "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                                }
+                                dem = 0;
                             }
-                            if (dem != 1) {
-                                Toast.makeText(getBaseContext(), "Tài khoản hoặc mật khẩu không chính xác!", Toast.LENGTH_SHORT).show();
-                            } else {
-                                Toast.makeText(getBaseContext(), "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
-                            }
-                            dem = 0;
                         }
                     }
                 }
             }
         });
-        
     }
 }
